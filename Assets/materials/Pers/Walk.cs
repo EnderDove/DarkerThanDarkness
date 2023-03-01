69f652da00c257da52e8fe7b7ccbecf2746e47e3
Danila Muryndin <122121837+compl3te@users.noreply.github.com>
2023-03-01 23:21:24 +0300

    prototype_Walk

---
+++ Assets/materials/Pers/Walk.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Vector3 _input;
+
+    private Rigidbody2D _rigidbody;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        _input = new Vector2(Input.GetAxis("Horizontal"), 0);
+        transform.position += _input * _speed * Time.deltaTime;
+    }
+}
